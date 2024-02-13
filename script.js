bebfae162c17c456b19c49abb6c12e56943cb4a8
Abdul565656 <abdullahkashif12b3@gmail.com>
2024-02-13 09:43:45 +0500

    work complete

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("ASSALLAM O ALEAKUM");
+prompt("WHAT IS YOUR NAME??");
+
+if ( prompt ) { prompt = ABDULLAH 
+
+alert("YOU ARE RIGHT");
+}
+
+const item = {
+    "ABDULLAH": true,
+     "hassan": null,
+     "muhib": false,
+
+}
